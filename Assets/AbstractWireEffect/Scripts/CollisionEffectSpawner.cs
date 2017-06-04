e5a98965372fdc5d592cbd9446a72bd92a544f81
Maxim Maslennikov <q.leklee@gmail.com>
2017-06-04 16:37:17 +0500

    [feature] create abstract wires effect

---
+++ Assets/AbstractWireEffect/Scripts/CollisionEffectSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.AbstractWireEffect.Scripts
+{
+    [RequireComponent(typeof(AbstractWireEffect))]
+    internal sealed class CollisionEffectSpawner : MonoBehaviour
+    {
+        public float MaxDistance;
+        public GameObject CollisionEffect;
+        public float LifeTime = 1f;
+
+        private bool _spawned;
+        private Transform _transform;
+        private AbstractWireEffect _abstractWireEffect;
+
+        private void Start()
+        {
+            _transform = transform;
+            _abstractWireEffect = GetComponent<AbstractWireEffect>();
+        }
+
+        private void Update()
+        {
+            RaycastHit hit;
+            if (!Physics.Raycast(_transform.position, -_transform.up, out hit, 1))
+                return;
+
+            var distance = Vector3.Distance(_transform.position, hit.point);
+
+            if (distance >= MaxDistance)
+            {
+                _spawned = false;
+                return;
+            }
+
+            if (_spawned)
+                return;
+
+            var impactEffect = Instantiate(CollisionEffect, hit.point, CollisionEffect.transform.rotation);
+            _abstractWireEffect.SetParticleSystem(impactEffect.GetComponent<ParticleSystem>());
+            Destroy(impactEffect, LifeTime);
+
+            _spawned = true;
+        }
+    }
+}
