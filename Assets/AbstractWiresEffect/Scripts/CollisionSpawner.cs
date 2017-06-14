82dbb4b578c44c246aec5d46db48fa65687e2789
Maxim Maslennikov <q.leklee@gmail.com>
2017-06-14 20:28:02 +0500

    [refactor] small refactoring

---
+++ Assets/AbstractWiresEffect/Scripts/CollisionSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Assets.AbstractWiresEffect.Scripts
+{
+    [RequireComponent(typeof(CollisionDetector))]
+    internal sealed class CollisionSpawner : MonoBehaviour
+    {
+        public GameObject CollisionEffect;
+        public float LifeTime = 1f;
+
+        private bool _isAlreadySpawned;
+        private CollisionDetector _collisionDetector;
+
+        private void Start()
+        {
+            _collisionDetector = GetComponent<CollisionDetector>();
+            _collisionDetector.CollisionDetected += OnCollisionDetected;
+        }
+
+        private void OnCollisionDetected(Vector3 hit)
+        {
+            if (_isAlreadySpawned)
+                return;
+
+            InstantiateImpactEffect(hit);
+
+            ////todo add decal
+            ////InstantiateDecal();
+        }
+
+        private void InstantiateImpactEffect(Vector3 pos)
+        {
+            var impactEffect = Instantiate(CollisionEffect, pos, CollisionEffect.transform.rotation);
+            impactEffect.transform.position = pos;
+
+            //todo add ivoke with delay and params
+            Destroy(impactEffect, LifeTime);
+            Invoke("Reset", LifeTime);
+
+            _isAlreadySpawned = true;
+        }
+
+        private void Reset()
+        {
+            _isAlreadySpawned = false;
+        }
+
+        private void OnDestroy()
+        {
+            _collisionDetector.CollisionDetected -= OnCollisionDetected;
+        }
+    }
+}
